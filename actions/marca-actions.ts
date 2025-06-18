96c2e2942b8bcc180790f260ee7e50ffe7097870
Caio Maqueise Alécio Pinheiro <caio.pinheiro@ifms.edu.br>
2025-06-17 22:05:58 -0300

    aula 17-06-2025

---
+++ actions/marca-actions.ts
@@ -0,0 +1,20 @@
+'use server'
+
+import { redirect } from "next/navigation"
+
+export async function criarMarca(formData:FormData){
+     console.log(formData)
+ let response =  fetch('http://localhost:3002/marcas',{
+        method:'POST',
+        body:JSON.stringify({nome:formData.get('nome')})
+    })
+   
+    redirect('/cadastro/marcas/')
+}
+export async function deletarMarca(formData:FormData){
+    let response =  fetch('http://localhost:3002/marcas',{
+        method:'DELETE',
+        body:JSON.stringify({nome:formData.get('id')})
+    })
+    redirect('/cadastro/marcas/')
+}
--- app/cadastro/marcas/marcas-form.tsx
+++ app/cadastro/marcas/marcas-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { criarMarca } from "@/actions/marca-actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -9,7 +10,7 @@ import Link from "next/link";
 export function MarcasForm() {
   return (
     <section className="mt-8">
-      <form className="max-w-2xl">
+      <form action={criarMarca} className="max-w-2xl">
         <div className="space-y-4">
           <Label htmlFor="nome">Nome</Label>
           <Input name="nome" />
@@ -20,7 +21,7 @@ export function MarcasForm() {
               Cancelar
             </Button>
           </Link>
-          <Button type="button">
+          <Button type="submit">
             <SaveIcon />
             Salvar
           </Button>
